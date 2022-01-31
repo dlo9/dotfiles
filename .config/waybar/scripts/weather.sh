449f8b094bee045dafcd080e1ce9079f9a27cecc
David Orchard <if_coding@fastmail.com>
2022-01-30 19:02:51 -0800

    Fix weather, mod, etc

--- .config/waybar/scripts/weather.sh
+++ .config/waybar/scripts/weather.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+# Format options: https://github.com/chubin/wttr.in
 
-LOC="$1"
-# HTML encode string as %20
-LOCATION=$(sed -e "s/ /%20/g" <<<"$LOC")
-content=$(curl -sS "https://thisdavej.azurewebsites.net/api/weather/current?loc=$LOCATION&deg=C")
-ICON=$(curl -s 'https://wttr.in/?format=1' | sed 's/[+0-9a-cA-Z°-]//g' )
-# echo $ICON
-TEMP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)"' | sed 's/"//g')
-TOOLTIP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)\n\(.skytext)"' | sed 's/"//g')
-CLASS=$(echo $content | jq .skytext)
-echo '{"text": "'$TEMP'", "tooltip": "'$ICON $TOOLTIP $LOC'", "class": '$CLASS' }'
+LOCATION="$1"
+WEATHER="$(curl -s "wttr.in/$LOCATION?format=%l|%c|%C|%t|%f|%w")"
+LOCATION=$(echo "$WEATHER" | awk -F '|' '{print $1}' | sed 's/,/, /g')
+ICON=$(echo "$WEATHER" | awk -F '|' '{print $2}')
+DESCRIPTION=$(echo "$WEATHER" | awk -F '|' '{print $3}')
+TEMP=$(echo "$WEATHER" | awk -F '|' '{print $4}')
+FEELS_LIKE=$(echo "$WEATHER" | awk -F '|' '{print $5}')
+WIND=$(echo "$WEATHER" | awk -F '|' '{print $6}')
 
+printf '{"text":"%s %s", "tooltip":"%s: %s, %s, %s", "class":"", "percentage":""}' "$FEELS_LIKE" "$ICON" "$LOCATION" "$DESCRIPTION" "$TEMP" "$WIND"
