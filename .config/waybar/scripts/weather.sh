bfc013b163f506f1ee5270124ea39cf5d408231e
David Orchard <if_coding@fastmail.com>
2022-02-06 20:04:58 -0800

    Finish waybar config

--- .config/waybar/scripts/weather.sh
+++ .config/waybar/scripts/weather.sh
@@ -2,7 +2,7 @@
 # Format options: https://github.com/chubin/wttr.in
 
 LOCATION="$1"
-WEATHER="$(curl -s "wttr.in/$LOCATION?format=%l|%c|%C|%t|%f|%w")"
+WEATHER="$(curl -s "wttr.in/$LOCATION?format=%l|%c|%C|%t|%f|%w" | sed -r -e 's/\s*\|\s*/|/g' -e 's/\+([0-9.]+°)/\1/g')"
 LOCATION=$(echo "$WEATHER" | awk -F '|' '{print $1}' | sed 's/,/, /g')
 ICON=$(echo "$WEATHER" | awk -F '|' '{print $2}')
 DESCRIPTION=$(echo "$WEATHER" | awk -F '|' '{print $3}')
@@ -10,4 +10,4 @@ TEMP=$(echo "$WEATHER" | awk -F '|' '{print $4}')
 FEELS_LIKE=$(echo "$WEATHER" | awk -F '|' '{print $5}')
 WIND=$(echo "$WEATHER" | awk -F '|' '{print $6}')
 
-printf '{"text":"%s %s", "tooltip":"%s: %s, %s, %s", "class":"", "percentage":""}' "$FEELS_LIKE" "$ICON" "$LOCATION" "$DESCRIPTION" "$TEMP" "$WIND"
+printf '{"text":"%s %s", "tooltip":"%s: %s, %s, %s", "class":"", "percentage":""}' "$ICON" "$FEELS_LIKE" "$LOCATION" "$DESCRIPTION" "$TEMP" "$WIND"
